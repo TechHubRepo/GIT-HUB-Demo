29a72447ca23b228c7111188417ea2d126f6f4e0
Ram Niwash <rniwash122@gmail.com>
2024-11-30 15:32:14 +0530

    Added Java Project

---
+++ src/main/java/com/techhub/github/demo/Start.java
@@ -0,0 +1,14 @@
+package com.techhub.github.demo;
+
+
+/**
+ * Application Starting class
+ *
+ * @author Ram Niwash
+ */
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD");
+    }
+}
